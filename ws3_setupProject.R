086a4212cda79f5199bc4c7ccf12654996caae0d
ianmseddy <ianmseddy@gmail.com>
2025-07-04 20:20:29 +0000

    use area contorl for now

--- ws3_setupProject.R
+++ ws3_setupProject.R
@@ -19,7 +19,7 @@ period_length <- 10 # do not modify this unless you know what you are doing
 tifPath <- "tif" # do not modify (works with included dataset)
 # outputs <-data.frame(objectName = "landscape") # do not modify
 # scheduler.mode <- "areacontrol" # self-tuning oldest-first priority queue heuristic algorithm (should "just work")
-scheduler.mode <- "optimize" # this should also "just work" (needs more testing)
+scheduler.mode <- "areacontrol" # this should also "just work" (needs more testing)
 target.masks <- list(c('? ? ? ?')) # do not modify
 target.scalefactors <- NULL
 shp.path <- "gis/shp"
@@ -44,28 +44,32 @@ out <- SpaDES.project::setupProject(
   times = list(start = 0, end = 200), # do not modify
   options = list(spades.allowInitDuringSimInit = TRUE),
   outputs = data.frame(objectName = "landscape"), # do not modify
-  params = list(spades_ws3_dataInit = list(basenames = basenames,
-                                           tifPath = tifPath,
-                                           base.year = base.year,
-                                           .saveInitialTime = 0,
-                                           .saveInterval = 1,
-                                           .saveObjects = c("landscape"),
-                                           .savePath = file.path(paths$outputPath, "landscape")),
-                spades_ws3 = list(basenames = basenames,
-                                  horizon = horizon,
-                                  period_length = period_length,
-                                  tif.path = tifPath,
-                                  shp.path = shp.path,
-                                  base.year = base.year,
-                                  scheduler.mode = scheduler.mode,
-                                  target.masks = target.masks,
-                                  target.scalefactors = target.scalefactors)
+  params = list(
+    .globals = list(basenames = basenames,
+                   tifPath = tifPath,
+                   base.year = base.year),
+    spades_ws3_dataInit = list(basenames = basenames,
+                               tifPath = tifPath,
+                               base.year = base.year,
+                               .saveInitialTime = 0,
+                               .saveInterval = 1,
+                               .saveObjects = c("landscape"),
+                               .savePath = file.path(paths$outputPath, "landscape")),
+    spades_ws3 = list(basenames = basenames,
+                      horizon = horizon,
+                      period_length = period_length,
+                      tif.path = tifPath,
+                      shp.path = shp.path,
+                      base.year = base.year,
+                      scheduler.mode = scheduler.mode,
+                      target.masks = target.masks,
+                      target.scalefactors = target.scalefactors)
   ),
   packages = c("gert", #"SpaDES",
                "reticulate", "httr",
-               "PredictiveEcology/reproducible@AI"
-               ),
-  require = "PredictiveEcology/SpaDES.core@box"
+               "PredictiveEcology/reproducible@AI",
+               "PredictiveEcology/SpaDES.core@box (>= 2.1.5.9005)"
+               )
 )
 
 out$modules <- c(grep("scfm", out$modules, invert = TRUE, value = TRUE),
@@ -102,59 +106,3 @@ simOut <- do.call(SpaDES.core::simInitAndSpades, out)
 #cgen_abrn_e is a burn constraint
 
 
-
-
-
-
-################################################################################
-if (FALSE) { # This was inherited from Greg's old code: it is entirely replaced by above
-  # set up SpaDES paths
-  setPaths(modulePath = 'modules',
-           inputPath = 'input',
-           outputPath = 'output',
-           cachePath = 'cache')
-  paths <- getPaths()
-  ################################################################################
-
-  ################################################################################
-  # define SpaDES module import list
-  modules <- list('spades_ws3_dataInit', 'bogus_fire', 'spades_ws3')
-  ################################################################################
-
-
-  ################################################################################
-  # define SpaDES params input arg data structure
-  sim <- SpaDES.core::simInit(paths=out$paths, modules=out$modules, times=times, params=params, outputs=outputs)
-  ################################################################################
-
-  ################################################################################
-  # run SpaDES simulation
-  simOut <- SpaDES.core::spades(sim, debug=TRUE)
-  #
-}
-
-################################################################################
-# plot something ?
-freq(simOut$landscape$age) # this is not a great example (do better)
-################################################################################
-
-################################################################################
-# example showing how it is possible to poke around the ws3 model directly
-# py$simulate_harvest(sim$fm, list(c("tsa41")), 2020)
-################################################################################
-
-
-################################################################################
-# compile some aggregated data tables from raw geotiff spades_ws3 model output
-#years <- 2020:2099
-#burned.area.tsa08 <- sapply(years, function(x){return(cellStats(rast(paste0('input/tif/tsa08/projected_fire_', x, '.tif')), sum) * 6.25)})
-#burned.area.tsa16 <- sapply(years, function(x){return(cellStats(rast(paste0('input/tif/tsa16/projected_fire_', x, '.tif')), sum) * 6.25)})
-#burned.area.tsa24 <- sapply(years, function(x){return(cellStats(rast(paste0('input/tif/tsa24/projected_fire_', x, '.tif')), sum) * 6.25)})
-#burned.area.tsa40 <- sapply(years, function(x){return(cellStats(rast(paste0('input/tif/tsa40/projected_fire_', x, '.tif')), sum) * 6.25)})
-#burned.area.tsa41 <- sapply(years, function(x){return(cellStats(rast(paste0('input/tif/tsa41/projected_fire_', x, '.tif')), sum) * 6.25)})
-#harvested.area.tsa08 <- sapply(years, function(x){return(cellStats(rast(paste0('input/tif/tsa08/projected_harvest_', x, '.tif')), sum) * 6.25)})
-#harvested.area.tsa16 <- sapply(years, function(x){return(cellStats(rast(paste0('input/tif/tsa16/projected_harvest_', x, '.tif')), sum) * 6.25)})
-#harvested.area.tsa24 <- sapply(years, function(x){return(cellStats(rast(paste0('input/tif/tsa24/projected_harvest_', x, '.tif')), sum) * 6.25)})
-#harvested.area.tsa40 <- sapply(years, function(x){return(cellStats(rast(paste0('input/tif/tsa40/projected_harvest_', x, '.tif')), sum) * 6.25)})
-#harvested.area.tsa41 <- sapply(years, function(x){return(cellStats(rast(paste0('input/tif/tsa41/projected_harvest_', x, '.tif')), sum) * 6.25)})
-################################################################################
